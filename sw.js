78d1c9367a7ebed9ee9bd5fd4419156750b2ae24
sEppofaz <josef.jf.fischer@me.com>
2026-03-19 15:22:09 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabeltrainer-v21';
+const CACHE = 'vokabeltrainer-v22';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {

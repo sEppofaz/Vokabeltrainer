bca15186adf9616e4324402748b85c3291635b4e
sEpp <josef.jf.fischer@me.com>
2026-08-14 17:12:16 +0200

    Tab-Leiste an unteren Bildschirmrand verschoben (PWA-Standard, smartphonegerecht)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabeltrainer-v49';
+const CACHE = 'vokabeltrainer-v50';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {

04a98e2b93f8be20a0b6a900af049fa8a29fb87e
sEpp <josef.jf.fischer@me.com>
2026-08-15 11:20:23 +0200

    Dark-/Hell-Modus-Umschalter im Hilfe-Sheet (BKM-Standard 2026-08-15)
    CSS-Override-Block fuer alle 4 bestehenden prefers-color-scheme-Bloecke
    (root-Variablen + 2 klassenbasierte Bloecke fuer Heatmap/Schwierig-Karten),
    Anti-Flacker-Script vor </head>, Toggle im Hilfe-Sheet oben. v50 -> v51.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vokabeltrainer-v50';
+const CACHE = 'vokabeltrainer-v51';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
